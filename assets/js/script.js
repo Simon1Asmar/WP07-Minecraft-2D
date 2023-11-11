315266af49e0095f8c065b437798e20010cd2669
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-11-11 03:54:19 +0200

    working on tools and inventory sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,27 @@
 //Selecting DOM Elements
 const grid = document.querySelector("#game-grid");
+const toolsSection = document.querySelector("#tools-section");
+
+grid.addEventListener("click", e=>{
+ 
+  console.log(e.target.id);
+})
+
+toolsSection.addEventListener("click", e=>{
+  console.log(e.target.id);
+})
 
 //TEMPORARY VALUES, when it's dynamic these values will be taken from a DOM element
 const numOfRows = 10;
-const numOfCols = 12;
+const numOfCols = 15;
 const cellSize = 100;
 
-buildGrid(numOfRows, numOfCols, cellSize);
+const game = {
+  surfaceLevel: Math.ceil(numOfRows * 0.5),
+}
+
+const gridData = buildGrid(numOfRows, numOfCols, cellSize);
+addBlocks(gridData);
 
 // creates and appends grid cells to the grid based on the number of rows and columns
 function buildGrid(numOfRows, numOfCols, cellSize){
@@ -14,12 +29,28 @@ function buildGrid(numOfRows, numOfCols, cellSize){
   grid.style.gridTemplateRows = `repeat(${numOfRows}, ${cellSize}px)`;
   grid.style.gridTemplateColumns = `repeat(${numOfCols}, ${cellSize}px)`;
 
+  const gridData = [];
+
   for (let row = 0; row < numOfRows; row++){
     for (let column = 0; column < numOfCols; column++){
       const cell = document.createElement("div");
       cell.classList.add("grid-cell");
-      cell.id = 
+      cell.id = `${row}-${column}`;
       grid.append(cell);
+      gridData.push(cell);
     }
   }
+
+  return gridData;
+}
+
+// Goes over each cell and sets it's time
+function addBlocks(){
+  gridData.forEach(cell => {
+    let [row, column] = cell.getAttribute("id").split("-");
+
+    if(row >= game.surfaceLevel){
+      cell.classList.add("dirt");
+    }
+  });
 }

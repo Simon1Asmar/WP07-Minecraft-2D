304622927668327b1b861d741b1c6fb424661b9c
Simon1Asmar <72914663+Simon1Asmar@users.noreply.github.com>
2023-11-11 11:52:11 +0200

    added breaking blocks functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,14 @@ const grid = document.querySelector("#game-grid");
 const toolsSection = document.querySelector("#tools-section");
 const inventorySection = document.querySelector("#inventory-section")
 
-grid.addEventListener("click", e=>{
- 
-  console.log(e.target.id);
-})
-
-toolsSection.addEventListener("click", e=>{
-  console.log(e.target.id);
-  e.target.classList.add("selected")
-})
-
-inventorySection.addEventListener("click", e=> {
-  console.log(e.target.id);
-  // console.log(e.target.classList[1]);
-  if(e.target.id){
-    e.target.classList.add("selected")
-  }
-})
+// grid.addEventListener("click", e=>{
+  
+  //   console.log(e.target.id);
+  // })
+  
+grid.addEventListener("click", gridEventListener);
+toolsSection.addEventListener("click", updateCurrentAction);
+inventorySection.addEventListener("click", updateCurrentAction);
 
 //TEMPORARY VALUES, when it's dynamic these values will be taken from a DOM element
 const game = {
@@ -58,6 +49,26 @@ const game = {
 
   // current item or tool
   currentAction: "",
+  checkIfBreakable: function(cell){
+    const pickaxeBreaks = ["stone-item"];
+    const axeBreaks = ["wood-item", "leaves-item"];
+    const shovelBreaks = ["dirt-item", "grass-item"];
+
+    if(
+      (this.currentAction === "pickaxe" && pickaxeBreaks.includes(cell.getAttribute("blockType")))
+    || (this.currentAction === "axe" && axeBreaks.includes(cell.getAttribute("blockType")))
+    || (this.currentAction === "shovel" && shovelBreaks.includes(cell.getAttribute("blockType")))
+    ){
+      console.log("YES");
+      return true;
+    } else {
+      console.log("NO");
+      return false;
+    }
+
+    
+
+  },
 
   // tree x / column locations
   treeXLocations: [],
@@ -71,12 +82,17 @@ const game = {
       }
     }
   },
+
+  // inventory
+  inventory: {
+    
+  }
 }
 
 game.calcSurfaceLevel();
 // game.currentTheme = game.themeOptions.normal;
-// game.currentTheme = game.themeOptions.snow;
-game.currentTheme = game.themeOptions.desert;
+game.currentTheme = game.themeOptions.snow;
+// game.currentTheme = game.themeOptions.desert;
 
 initializeInventory();
 
@@ -87,6 +103,8 @@ function initializeInventory(){
   let currentThemeKeys = Object.keys(game.currentTheme);
   console.log('currentThemeClassList', currentThemeClassList)
 
+  game.inventory = {};
+
   for (let i=0; i<inventoryChildren.length; i++){
 
     const p = inventoryChildren[i].querySelector("p");
@@ -95,12 +113,16 @@ function initializeInventory(){
       inventoryChildren[i].id = `${currentThemeKeys[i]}-item`
       inventoryChildren[i].classList.add(currentThemeClassList[i]);
 
-      p.textContent = "0";
+      game.inventory[currentThemeKeys[i]] = 0
+
+      // p.textContent = "0";
     } else {
       
       p.textContent = "";
     }
   }
+  updateItemCountersUI();
+
 
 }
 
@@ -141,33 +163,142 @@ function addBlocks(){
   });
 }
 
+// draws blocks based on their location in the grid
 function checkTypeAndDraw(row, column, cell){
   if(row === game.surfaceLevel){
     // Draws grass at surface level
     if(game.currentTheme.grass){
       cell.classList.add(game.currentTheme.grass);
+      cell.setAttribute("blockType", "grass-item");
+      console.log('cell', cell)
+      // cell.setAttribue("block-type", `grass-item`);
     } else {
       // in themes such as desert we have sand for surface and below surface level
       cell.classList.add(game.currentTheme.dirt);
+      cell.setAttribute("blockType", "dirt-item");
     }
   } else if (row > game.surfaceLevel){
     // Draws random elements below surface level
     const randomNum = Math.floor(Math.random() * 10);
     if(randomNum>=7){
       cell.classList.add(game.currentTheme.stone);
+      cell.setAttribute("blockType", "stone-item");
     } else {
       cell.classList.add(game.currentTheme.dirt)
+      cell.setAttribute("blockType", "dirt-item");
     }
   } else if(game.treeXLocations.includes(column) && (row < game.surfaceLevel && row > game.surfaceLevel - 4)){
     // draws wood and leaves on the x location of a tree
     if(row === game.surfaceLevel - 3){
       cell.classList.add(game.currentTheme.leaves)
+      cell.setAttribute("blockType", "leaves-item");
     } else {
       cell.classList.add(game.currentTheme.wood)
+      cell.setAttribute("blockType", "wood-item");
     }
   }
   else if((row === game.surfaceLevel - 2 || row === game.surfaceLevel - 3) && (game.treeXLocations.includes(column-1) || game.treeXLocations.includes(column+1)) && game.currentTheme.leaves != null){
     //draws leaves on the sides of trees
     cell.classList.add(game.currentTheme.leaves)
+    cell.setAttribute("blockType", "leaves-item");
+  }
+}
+
+// removes the selected styling class from all items and inventory elements
+function resetSelectedStyling(){
+  [...inventorySection.children].forEach(child => {
+      child.classList.remove("selected");
+  });
+
+  [...toolsSection.children].forEach(child => {
+    child.classList.remove("selected");
+  });
+}
+
+// updates game's current action
+function updateCurrentAction(event){
+  if(event.target.classList[0] === "tool" || (event.target.classList[0] === "item" && event.target.id)){
+    resetSelectedStyling();
+    event.target.classList.add("selected");
+    game.currentAction = event.target.id;
+    console.log('game.currentAction = event.target.id;', game.currentAction = event.target.id)
+  }
+}
+
+function gridEventListener(event){
+  
+  if(event.target.classList.contains("grid-cell") && !event.target.classList.contains("cloud") && event.target.classList.length > 1){
+    
+    const cell = event.target;
+    console.log('game.currentAction', game.currentAction)
+    console.log('cell', cell)
+
+    // game.checkIfBreakable(cell);
+
+    if(game.checkIfBreakable(cell)){
+      console.log(`removing ${cell.getAttribute("blockType")} from ${cell.classList}`);
+      cell.classList.remove(cell.classList[1]);
+      const blockType = cell.getAttribute("blockType");
+      cell.setAttribute("blockType", "");
+      game.inventory[blockType.substring(0, blockType.lastIndexOf("-"))]++;
+    }
+
+    // switch (game.currentAction) {
+    //   case "axe":
+    //     // BREAK WOOD AND LEAVES
+    //     const blockType = cell.classList[1];
+    //     if(blockType.includes("log") || blockType.includes("cactus")){
+    //       inventorySection.querySelector("#wood-item");
+    //       game.inventory.wood ++;
+    //       cell.classList.remove(blockType);
+    //     } else if (blockType.includes("leaves")){
+    //       inventorySection.querySelector("#leaves-item");
+    //       game.inventory.leaves ++;
+    //       cell.classList.remove(blockType);
+    //     }
+    //     break;
+    //   case "pickaxe":
+    //     // break stone
+    //     if(game.checkIfBreakable())
+    //     break;
+    //   case "shovel":
+    //     // break sand
+    //     break;
+    //   case "dirt-item":
+    //     // add dirt block
+    //     break;
+    //   case "grass-item":
+    //     // add grass block
+    //     break;
+    //   case "stone-item":
+    //     // add stone block
+    //     break;
+    //   case "wood-item":
+    //     // add wood block
+    //     break;
+    //   case "leaves-item":
+    //     // add leaves block
+    //     break;
+    
+    //   default:
+    //     break;
+    // }
+   
+  }
+
+  updateItemCountersUI();
+
+
+}
+
+function updateItemCountersUI(){
+  const keys = Object.keys(game.inventory);
+  // console.log('keys', keys)
+  const values = Object.values(game.inventory);
+  // console.log('values', values)
+
+  for (let i = 0; i < keys.length; i++) {
+    const p = inventorySection.querySelector(`#${keys[i]}-item>p`);
+    p.textContent = game.inventory[keys[i]];
   }
 }
